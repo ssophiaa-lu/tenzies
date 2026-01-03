6d7105dd32f2510c6b750822a05aa23744d67013
Sophia Lu <ssophiaa.lu@gmail.com>
2026-01-02 21:42:48 -0500

    clean up comments

--- App.jsx
+++ App.jsx
@@ -3,14 +3,7 @@ import Die from "./Die"
 import { nanoid } from "nanoid"
 
 export default function App() {
-    /**
-     * Challenge: Add conditional styling to the Die component
-     * so that if it's held (isHeld === true), its background color
-     * changes to a light green (#59E391)
-     * 
-     * Remember: currently the Die component has no way of knowing
-     * if it's "held" or not.
-     */
+   
     
     const [dice, setDice] = useState(generateAllNewDice())
 
